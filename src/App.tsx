a38975d00f3b0f719307eba67c3f100103e0b555
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-06-20 23:03:11 +0900

    簡易リンクとルーティングを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
+import { BrowserRouter, Switch, Link, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { News } from "./pages/News";
+import { Setting } from "./pages/Setting";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/news">News</Link></li>
+          <li><Link to="/setting">Setting</Link></li>
+        </ul>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/news">
+            <News />
+          </Route>
+          <Route path="/setting">
+            <Setting />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+      
     </div>
   );
 }
